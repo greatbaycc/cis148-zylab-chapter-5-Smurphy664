427e2a796146bd329e66120e67d0b7ebc59d4137
Sean Murphy <smurph11223344@gmail.com>
2024-05-01 23:00:06 -0400

    VariedInputData.java

--- src/VariedInputData.java
+++ src/VariedInputData.java
@@ -2,6 +2,25 @@ import java.util.Scanner;
 
 public class VariedInputData {
     public static void main(String[] args) {
-        /* Type your code here. */
+        Scanner scnr = new Scanner(System.in);
+
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int count = 0;
+
+        while (true) {
+            int num = scnr.nextInt();
+            if (num < 0) {
+                break;
+            }
+            sum += num;
+            max = Math.max(max, num);
+            count++;
+        }
+
+        double average = (double) sum / count;
+        System.out.println((int) average + " " + max);
     }
 }
+
+
