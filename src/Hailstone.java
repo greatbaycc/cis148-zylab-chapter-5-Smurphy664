1cda168f084e0cd0066ee1f9002e5cac638f2bc9
Sean Murphy <smurph11223344@gmail.com>
2024-05-01 22:49:57 -0400

    Hailstone.java

--- src/Hailstone.java
+++ src/Hailstone.java
@@ -1,17 +1,31 @@
 import java.util.Scanner;
 
 public class Hailstone {
-    public int hailstone(int num) {
-        /* Type your code here. */
-        return 0; //replace this when ready to work on this method
+
+    public void hailstone(int num) {
+        int count = 0;
+        while (num != 1) {
+            System.out.print(num + "\t");
+            count++;
+            if (count == 10) {
+                System.out.println();
+                count = 0;
+            }
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num = 3 * num + 1;
+            }
+        }
+        System.out.print(num);
     }
 
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         Hailstone labObject = new Hailstone();
         int num;
-
         num = scnr.nextInt();
-        labObject.hailstone(num); // Call hailstone() to print out the hailstone sequence.
+        labObject.hailstone(num);
+        System.out.println();
     }
 }
