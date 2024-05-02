60de98fa7c506307288a9bfd838510840715dfbf
Sean Murphy <smurph11223344@gmail.com>
2024-05-01 22:58:39 -0400

    RemoveSpaces.java

--- src/StringReverser.java
+++ src/StringReverser.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class StringReverser {
     public static void main(String[] args) {
-        /* Type your code here. */
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("quit") || input.equalsIgnoreCase("q")) {
+                break;
+            }
+
+            String reversed = new StringBuilder(input).reverse().toString();
+            System.out.println(reversed);
+        }
     }
 }
+
+
