920da0bd2ce9731bd24acc6aeec9d901e9e6b674
Sean Murphy <smurph11223344@gmail.com>
2024-05-01 22:42:51 -0400

    CharacterCounter.java

--- src/CharacterCounter.java
+++ src/CharacterCounter.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class CharacterCounter {
     public static void main(String[] args) {
-        /* Type your code here. */
+        Scanner scanner = new Scanner(System.in);
+
+        char character = scanner.next().charAt(0);
+        String input = scanner.nextLine();
+
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == character) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
     }
 }
+
+
