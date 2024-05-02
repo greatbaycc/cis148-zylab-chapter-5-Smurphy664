2049e194b9e1671c5cf04dc3c29e73e58d1468f2
Sean Murphy <smurph11223344@gmail.com>
2024-05-01 22:57:56 -0400

    RemoveSpaces.java

--- src/RemoveSpaces.java
+++ src/RemoveSpaces.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class RemoveSpaces {
     public static void main(String[] args) {
-        /* Type your code here. */
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String output = input.replaceAll(" ", "");
+
+        System.out.println(output);
     }
 }
+
+
